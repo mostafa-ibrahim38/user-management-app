a8d5390ab5460a141d1913cf24fe7991daf57f94
Mostafa Ibrahim <mostafa.ibrahim.dev38@gmail.com>
2026-07-03 16:05:36 +0300

    feat: implement validation logic in ValidateUserUseCase[GREEN]

--- app/src/main/java/com/trendsoftware/usermanagementapp/domain/usecase/ValidateUserUseCase.kt
+++ app/src/main/java/com/trendsoftware/usermanagementapp/domain/usecase/ValidateUserUseCase.kt
@@ -3,11 +3,22 @@ package com.trendsoftware.usermanagementapp.domain.usecase
 import com.trendsoftware.usermanagementapp.domain.entity.User
 import com.trendsoftware.usermanagementapp.domain.validation.UserValidator
 import com.trendsoftware.usermanagementapp.domain.validation.ValidationResult
+import javax.inject.Inject
 
-class ValidateUserUseCase(
+class ValidateUserUseCase @Inject constructor(
     private val validator: UserValidator
 ) {
     operator fun invoke(user: User): ValidationResult {
-        TODO("Not yet implemented")
+        listOf(
+            validator.validateName(user.name),
+            validator.validateAge(user.age),
+            validator.validateJobTitle(user.jobTitle),
+            validator.validateGender(user.gender)
+        ).forEach { result ->
+            if (result is ValidationResult.Error) {
+                return result
+            }
+        }
+        return ValidationResult.Success
     }
 }
