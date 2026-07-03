7ed8dd6fa046a265696407eacf6c3b75709f7193
Mostafa Ibrahim <mostafa.ibrahim.dev38@gmail.com>
2026-07-03 17:25:18 +0300

    feat: implement user addition and retrieval logic in UserRepositoryImpl [GREEN]

--- app/src/main/java/com/trendsoftware/usermanagementapp/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/trendsoftware/usermanagementapp/data/repository/UserRepositoryImpl.kt
@@ -1,9 +1,12 @@
 package com.trendsoftware.usermanagementapp.data.repository
 
 import com.trendsoftware.usermanagementapp.data.local.dao.UserDao
+import com.trendsoftware.usermanagementapp.data.mapper.toDomain
+import com.trendsoftware.usermanagementapp.data.mapper.toEntity
 import com.trendsoftware.usermanagementapp.domain.entity.User
 import com.trendsoftware.usermanagementapp.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
@@ -11,10 +14,14 @@ class UserRepositoryImpl @Inject constructor(
 ) : UserRepository {
 
     override suspend fun addUser(user: User) {
-        TODO("Not yet implemented")
+        userDao.insertUser(user.toEntity())
     }
 
     override fun getAllUsers(): Flow<List<User>> {
-        TODO("Not yet implemented")
+        return userDao
+            .getAllUsers()
+            .map { users ->
+                users.map { it.toDomain() }
+            }
     }
 }
