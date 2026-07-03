c8bffad7dfd21477f543995156598915401d42ff
Mostafa Ibrahim <mostafa.ibrahim.dev38@gmail.com>
2026-07-03 16:29:37 +0300

    feat: implement user addition logic with validation in AddUserUseCase [GREEN]

--- app/src/main/java/com/trendsoftware/usermanagementapp/domain/usecase/AddUserUseCase.kt
+++ app/src/main/java/com/trendsoftware/usermanagementapp/domain/usecase/AddUserUseCase.kt
@@ -3,13 +3,27 @@ package com.trendsoftware.usermanagementapp.domain.usecase
 import com.trendsoftware.usermanagementapp.domain.entity.User
 import com.trendsoftware.usermanagementapp.domain.repository.UserRepository
 import com.trendsoftware.usermanagementapp.domain.result.AddUserResult
+import com.trendsoftware.usermanagementapp.domain.validation.ValidationResult
+import javax.inject.Inject
 
 
-class AddUserUseCase(
+class AddUserUseCase @Inject constructor(
     private val repository: UserRepository,
     private val validateUserUseCase: ValidateUserUseCase
 ) {
     suspend operator fun invoke(user: User): AddUserResult {
-        TODO("Not yet implemented")
+        return when (val validation = validateUserUseCase(user)) {
+            ValidationResult.Success -> {
+                try {
+                    repository.addUser(user)
+                    AddUserResult.Success
+                } catch (e: Exception) {
+                    AddUserResult.DatabaseError
+                }
+            }
+            is ValidationResult.Error -> {
+                AddUserResult.ValidationFailed(validation.error)
+            }
+        }
     }
 }
