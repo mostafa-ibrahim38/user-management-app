1ed05d0a1d573ecaef89da71acf6a8d57966402b
Mostafa Ibrahim <mostafa.ibrahim.dev38@gmail.com>
2026-07-04 03:48:43 +0300

    feat: add DisplayUiState data class for managing user display state

---
+++ app/src/main/java/com/trendsoftware/usermanagementapp/ui/display/DisplayUiState.kt
@@ -0,0 +1,9 @@
+package com.trendsoftware.usermanagementapp.ui.display
+
+import com.trendsoftware.usermanagementapp.domain.entity.User
+
+
+data class DisplayUiState(
+    val users: List<User> = emptyList(),
+    val isLoading: Boolean = false
+)
